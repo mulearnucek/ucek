e52f11c5b62d33c51e5b5219bdb3d77b1fec3503
ABHISHEK <abhishekp9a@gmail.com>
2025-05-02 13:33:16 +0530

    update

--- components/layout/popup.jsx
+++ components/layout/popup.jsx
@@ -45,7 +45,7 @@ const Popup = () => {
                 className="w-screen h-screen bg-[rgba(0,0,0,0.8)] fixed top-0 left-0 flex items-center justify-center z-[9999] transition-opacity duration-300 ease-in-out opacity-0 pointer-events-none">
                 <div
                     id="popupBox"
-                    className="w-[70vh] h-[40vh] text-white bg-black flex items-center justify-center relative opacity-0 scale-95 transition-all duration-300 ease-out">
+                    className="w-[80%] max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl text-white bg-black p-6 flex items-center justify-center relative opacity-0 scale-95 transition-all duration-300 ease-out rounded-xl">
                     <button
                         id="closeBtn"
                         className="absolute top-3 right-3 z-50 h-8 w-8 flex justify-center text-white bg-gray-500 bg-opacity-40 rounded-full text-xl hover:scale-110 hover:bg-opacity-90 transition-transform duration-200 ease-in-out focus:outline-none select-none">
